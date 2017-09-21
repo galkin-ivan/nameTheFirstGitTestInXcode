cf88b7268f6a0e4286e49d4e16214c3cd532c4a7
galkinivanMac <galkinivansergeevich@gmail.com>
2017-09-21 15:58:42 +0300

    did smthg

--- name/AppDelegate.m
+++ name/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // maybe i'll do sthg here)))
     return YES;
 }
 
